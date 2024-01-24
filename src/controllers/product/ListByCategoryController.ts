ff8bdbd526c6dc02a3855ba2f468f71408f4c335
ianabreu <ianabreudev@gmail.com>
2024-01-24 06:02:45 -0300

    fix: ensure category_id is not undefined

--- src/controllers/product/ListByCategoryController.ts
+++ src/controllers/product/ListByCategoryController.ts
@@ -4,6 +4,9 @@ import { ListByCategoryService } from "../../services/product/ListByCategoryServ
 const ListByCategoryController = {
   handle: async (request: Request, response: Response) => {
     const category_id = request.query.category_id as string;
+    if (!category_id || category_id === undefined) {
+      throw new Error("category id is required");
+    }
     const findByCategory = await ListByCategoryService.execute({ category_id });
     return response.status(200).json(findByCategory);
   },
